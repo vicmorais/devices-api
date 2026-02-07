f103dad99f531818dddabe13225367505104eaa8
Victor Morais <vmorais@mainhub.pt>
2026-02-07 14:11:55 +0000

    feat add Mappings Dtos - Entities

--- devices-api/Devices/src/Devices.Infrastructure/Entities/Device.cs
+++ devices-api/Devices/src/Devices.Infrastructure/Entities/Device.cs
@@ -1,7 +1,4 @@
 ﻿using Devices.Application.Enums;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Devices.Infrastructure.Entities;
 
---
+++ devices-api/Devices/src/Devices.Infrastructure/Mappings/DeviceMappingExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Devices.Application.DTOs;
+using Devices.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Devices.Infrastructure.Mappings;
+public static class DeviceMappingExtensions
+{
+    public static Device ToEntity(this CreateDeviceDto dto)
+    {
+        return new Device
+        {
+            Name=dto.Name,
+            Brand=dto.Brand,
+            State=dto.State
+        };
+    }
+
+    public static DeviceDto ToDto(this Device entity)
+    {
+        return new DeviceDto
+        {
+            Id = entity.Id,
+            State = entity.State,
+            Brand = entity.Brand,
+            Name = entity.Name, 
+            CreationTime = entity.CreationTime,
+            LastUpdatedTime = entity.LastUpdateTime
+        };
+    }
+
+    public static IEnumerable<DeviceDto> ToDtos(this IEnumerable<Device> entities)
+    {
+        return entities.Select(e => e.ToDto());
+    }
+}
